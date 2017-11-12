28b63bfbec272b5ecd0cc070d7f3f32e49bad034
Mieq <cmk0292@yahoo.com>
2017-11-12 22:05:25 +0000

    Update fz.ts

--- fz.ts
+++ fz.ts
@@ -6,7 +6,7 @@ function fz() {
         var sudo = magik.getPlugin().getServer().getConsoleSender();
         magik.getPlugin().getServer().dispatchCommand(sudo, cmd);
     }
-    const flyingZombieCmd = 'tp ${name} -333 69 56';
+    const flyingZombieCmd = 'tp @p -333 69 56';
 
 const activeMessageCmd = 'tellraw @p ["\u00A78(\u00A7a!\u00A78) \u00A77Good luck, Have Fun!"]';
 
