cb1858b584e654534ab73c6735ad673ed5bec781
Mieq <cmk0292@yahoo.com>
2017-11-12 21:41:18 +0000

    Update countdown.ts

--- countdown.ts
+++ countdown.ts
@@ -3,5 +3,5 @@ const magik = magikcraft.io;
 function countdown(time = 5) {
     const bars = magik.Bars.addBar(magik.getSender(), magik.TextComponent("Countdown!"), magik.Bars.Color.GREEN, magik.Bars.Style.NOTCHED_20, 0.99);
     let progress = 0.99;
-    magik.setInterval(()=> { bars.setProgress(progress); progress -= 0.02 }, 1000);
+    magik.setInterval(()=> { bars.setProgress(progress); progress -= 1.0 }, 1000);
 }
