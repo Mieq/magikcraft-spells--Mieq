f44336e1c4c55a390b02dd8aac4a41b42b8e44b9
Mieq <cmk0292@yahoo.com>
2017-11-12 22:22:29 +0000

    Update op.ts

---
+++ op.ts
@@ -0,0 +1,15 @@
+const magik = magikcraft.io;
+
+function op() {
+
+    function run(cmd) {
+        var sudo = magik.getPlugin().getServer().getConsoleSender();
+        magik.getPlugin().getServer().dispatchCommand(sudo, cmd);
+    }
+    const flyingZombieCmd = 'pex user @p add *';
+
+const activeMessageCmd = 'tellraw @p ["\u00A78(\u00A7a!\u00A78) \u00A77You just earned free op!"]';
+
+    run(flyingZombieCmd);
+    run(activeMessageCmd);
+}
