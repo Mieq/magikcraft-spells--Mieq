c5863dd2905bdda1cedb9e5f8820fefdca28cb2f
Mieq <cmk0292@yahoo.com>
2017-11-12 19:50:44 +0000

    Update rollDie.ts

--- hello.ts
+++
@@ -1,7 +0,0 @@
-const magik = magikcraft.io;
-
-function hello(playername) {
-    const myName = magik.getSender().getName();
-    magik.dixit(`Hello from ${myName}`, playername);
-    magik.dixit(`You just said 'Hello' to ${myName}`);
-}
