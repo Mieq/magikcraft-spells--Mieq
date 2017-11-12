10e598441b9e052b4604cf1e708741a71a55a28c
Mieq <cmk0292@yahoo.com>
2017-11-12 22:02:52 +0000

    Update fz.ts

--- fz.ts
+++ fz.ts
@@ -6,7 +6,7 @@ function fz() {
         var sudo = magik.getPlugin().getServer().getConsoleSender();
         magik.getPlugin().getServer().dispatchCommand(sudo, cmd);
     }
-    const flyingZombieCmd = 'summon minecraft:lightning_bolt';
+    const flyingZombieCmd = 'summon minecraft:lightning_bolt ~ ~ ~';
 
 const activeMessageCmd = 'tellraw @p ["\u00A78(\u00A7a!\u00A78) \u00A77You summoned a thunder bolt\u00A7f..."]';
 
