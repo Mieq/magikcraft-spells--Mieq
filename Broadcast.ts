19c657f45b4d9c84de679d03f2cecb96724ab958
Mieq <cmk0292@yahoo.com>
2017-11-13 03:18:45 +0000

    Update Broadcast.ts

--- Broadcast.ts
+++ Broadcast.ts
@@ -1,12 +1,12 @@
 const magik = magikcraft.io;
 
-function BC() {
+function Broadcast() {
 
     function run(cmd) {
         var sudo = magik.getPlugin().getServer().getConsoleSender();
         magik.getPlugin().getServer().dispatchCommand(sudo, cmd);
     }
-    const flyingZombieCmd = 'tellraw @a ["\u00A78(\u00A74ALERT) \u00A77${message}"]';
+    const flyingZombieCmd = 'tellraw Pierro89 ["\u00A78(\u00A74ALERT) \u00A77${message}"]';
 
 
     run(flyingZombieCmd);
